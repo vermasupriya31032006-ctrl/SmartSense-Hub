f826c3af3f80009e68413eec50ef7616f805729d
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-08 16:58:55 +0530

    Create sketch.ino

---
+++ Sensor-Data-Simulation/code/sketch.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+
+#define DHTPIN 15
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.println("---------------------");
+
+  delay(2000);
+}
